23890852e3a1dfa9bf1197dad95fdd273d7f1aa2
Liri-39 <lirina74@gmail.com>
2020-09-13 17:29:51 +0200

    шапка, форма, правый отступ

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,26 +1,19 @@
 var navMain = document.querySelector('.main-nav');
-var navMainTop = document.querySelector('.main-nav-top');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navToggle = document.querySelector('.toggle-menu-button');
 
-navToggle.classList.remove('main-nav__toggle--nojs');
 navMain.classList.remove('main-nav--nojs');
-navMainTop.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
 
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
-    navMainTop.classList.remove('main-nav--closed');
-    navMainTop.classList.add('main-nav--opened');
-    navToggle.classList.remove('main-nav__toggle--open');
-    navToggle.classList.add('main-nav__toggle--close');
+    navToggle.classList.remove('toggle-menu-button--open');
+    navToggle.classList.add('toggle-menu-button--close');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
-    navMainTop.classList.add('main-nav--closed');
-    navMainTop.classList.remove('main-nav--opened');
-    navToggle.classList.remove('main-nav__toggle--close');
-    navToggle.classList.add('main-nav__toggle--open');
+    navToggle.classList.remove('toggle-menu-button--close');
+    navToggle.classList.add('toggle-menu-button--open');
   }
 });
